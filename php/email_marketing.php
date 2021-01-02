efa2206f4a2cd777b8a7bd575218a302774bb813
Leonardo Carlassare <lio.del.bronx@gmail.com>
2021-01-02 12:25:33 +0100

    Update email_marketing

--- php/email_marketing.php
+++ php/email_marketing.php
@@ -28,11 +28,16 @@ if(isset($_GET['Password']))
                     echo $row['Email'] . '<BR>';
                     
                     $to = $row['Email'];
-                    $subject = "";
-                    $message = "Ciao ".$row['Real_name'].", sono Leonardo, il programmatore che sta dietro Collection Sight!
-                    
+                    $subject = "Sito Reso Gratuito per tutti";
+                    $message = "Ciao ".$row['Real_name'].", sono Leonardo, il programmatore dietro Collection Sight.
+Volevo dirti che ho eliminato tutto il discorso dell'abbonamento al sito e ora è completamente gratuito per tutti.
+Chi lo aveva pagato é stato completamente rimborsato su PayPal.
+Colgo l’occasione per anticiparti che nei prossimi mesi ci sarà un sostanziale aggiornamento con il quale renderò più intuitivo il meccanismo di inserimento delle carte.
+Per qualsiasi suggerimento scrivimi a questa e-mail.
+Ti ringrazio per il tuo supporto e ti auguro un felice 2021. 
+    
 
-                    Sinceramente, Leonardo";
+Leonardo";
 
                                     
                    
@@ -64,8 +69,7 @@ if(isset($_GET['Password']))
                                 
                             $mail->Send();
                             //PUT THE USER TO THE VERIFICATION PAGE
-                            header("Location: ../verification_page.php?SIGNUP=SUCCESS");
-                            exit();
+                            echo 'Email mandate con succeso'.'<BR>';
                         } catch (phpmailerException $e) {
                             echo $e->errorMessage(); //Pretty error messages from PHPMailer
                         } catch (Exception $e) {
