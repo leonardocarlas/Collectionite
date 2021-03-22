8e2d6959c120333fae1a4196b760b24832254726
Leonardo Carlassare <lio.del.bronx@gmail.com>
2021-03-22 16:04:19 +0100

    Album.php stable

--- album.php
+++ album.php
@@ -19,22 +19,6 @@
         $_SESSION['idalbum'] = $id_album;    
     }
 
-    
-
-    if( isset( $_POST['EV']) AND $_POST['EV']=="Minimum & Trend Prices"){
-        $ricarica_prezzi_min_trend = TRUE;
-    }
-    if( isset( $_POST['EV']) AND $_POST['EV']=="Evaluation Prices based on Language & Condition"){
-        $ricarica_prezzi_lan_cond = TRUE;
-    }
-    if(isset( $_POST['INS']) AND $_POST['INS']=="Set & Name"){
-        $inserimento_per_set_nome = TRUE;
-        $_SESSION['ONLY-LINK'] = FALSE;
-    }
-    if(isset( $_POST['INS']) AND $_POST['INS']=="Only Link"){
-        $inserimento_per_link = TRUE;
-        $_SESSION['ONLY-LINK'] = TRUE;
-    }
 
 
     //VARIABILI GLOBALI
@@ -46,7 +30,7 @@
 
 ?>
 
-<!-- 1. Time Line -->
+<!-- Time Line -->
 
     <div class="row mb-2">
         <div class = "col">
@@ -54,27 +38,25 @@
         </div>   
     </div>
             
-<!-- 1.E FINISH -->
 
 
 
-<!-- 2.E E NORMAL INSERT NEW CARD -->
-<?php
-/*
-    require "add_card.php";
-*/
-?>
+
+
+
+<!-- Bottone che manda a new_add_card.php per far inserire all'utente le carte -->
+
 <br><br>
 <div class="row justify-content-center">
     <div class="col-auto-sm">
         <a class="btn text-white" href="new_add_card.php" style="background-color: #5401a7;">
-        <h4>Aggiungi nuove carte all'album</h4>
-        <img src="immagini/tre_carte.png" width="30" height="30">
+            <h4>Aggiungi nuove carte all'album</h4>
+            <img src="immagini/tre_carte.png" width="30" height="30">
         </a>
     </div>
 </div>
 
-<!-- 2.E FINISH -->
+<br><br>
 
 
 
@@ -138,6 +120,7 @@
                     <thead>
                         <tr>
                             <th scope="col">#</th>
+                            <th scope="col">Image</th>
                             <th scope="col">Carta</th>
                             <th scope="col">Espansione</th>
                             <th scope="col">Min Price</th>
@@ -156,27 +139,26 @@
                         <?php for($i = 0; $i < count($array_carte); $i = $i + 10) { ?>
                             <tr>
                                 <td> <?php echo $i/10 ?></td>
+                                <td> <?php echo '<img src="'.$array_carte[$i+2] .'" alt="Foto" width="100" height="150">'?></td>
                                 <td> <?php echo $array_carte[$i] ?></td>
                                 <td> <?php echo $array_carte[$i+1] ?></td>
-                                <td> <?php echo $array_carte[$i+3] ?></td>
-                                <td> <?php echo $array_carte[$i+4] ?></td>
-                                <td> <?php echo $array_carte[$i+5] ?></td>
+                                <td> <?php echo $array_carte[$i+3];  ?></td>
+                                <td> <?php echo $array_carte[$i+4];  $total_trend += $array_carte[$i+4]; ?></td>
+                                <td> <?php echo $array_carte[$i+5];  $total_min += $array_carte[$i+5];  ?></td>
                                 <td> <?php echo $array_carte[$i+6] ?></td>
                                 <td> <?php echo $array_carte[$i+7] ?></td>
                                 <td> <?php echo $array_carte[$i+8] ?></td>
                                 <td> <?php echo " - "?></td>
                                 <td> <?php $link = "https://www.cardmarket.com" . $array_carte[$i+9];  echo '<a href="'.$link.'"> link </a>'; ?></td>
                                 <td> <?php echo 'Elimina - Modifica ' ?></td>
-                            
                             </tr>
                         <?php } ?>
-                    </tbody>
+                        </tbody>
                 </table> 
             </div><!-- /.col-sm-12 -->
         </div><!-- /.row -->
     </div><!-- /.wrapper -->
 
-    
 <?php 
     } 
 
@@ -185,18 +167,103 @@
 
 
 
+<!-- Sommario -->
+
+<br>
+
+<div class="row justify-content-center">
+    <h2>Sommario</h2>
+</div>
+<div class="row justify-content-center">
+    <h5>Prezzo totale dell'album</h5>
+</div>
+
+<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col-md-auto">
+            Prezzo minimo: <?php echo $total_min; ?> €
+        </div>   
+        <div class="col-md-auto">
+            Prezzo di tendenza: <?php echo $total_trend; ?> €
+        </div>
+        <div class="col-md-auto">
+            Prezzo di valutazione: <?php echo $total_avarage; ?> €
+        </div>     
+    </div>
+</div>
 
 
 
 
 
-<!-- 5.E VMODE & NORMAL. GRAFICO-->
+<!-- Strumenti per l'album -->
+
+<br><br><br>
+
+    <div class="row justify-content-center">
+        <h2>Strumenti per l'album</h2>
+    </div>
+    
+    <br>
+
+    <div class="row justify-content-center">
+        <h5><p class="font-weight-bold">Comincia a tracciare il tuo album.</p></h5>
+    </div>
+    <div class="row justify-content-center">
+        <p>Ti consigliamo di cliccare il bottone se e solo se il tuo Album è completo e per un periodo di tempo non dovrai aggiungere altre carte.</p>
+        <br>
+        <p>
+        In questo modo il sito potrà disegnare un grafico davvero indicativo dell'andamento dei prezzi del tuo album.
+        </p>
+        
+    </div>
+    <div class="row justify-content-center">
+        <button type="submit" class="btn btn-link" name="start-track">
+                <?php
+                    $start_track = "php/start_tracking.php?start-track=" . $id_album ;
+                    echo '<a class="btn text-white" style="background-color: #5401a7;" href="'.$start_track.'">Start Tracking</a> '; 
+                ?>
+        </button>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <h5><p class="font-weight-bold">Registra il valore totale dell'album (prezzo minimo e di tendenza).</p></h5>
+    </div>
+    <div class="row justify-content-center">
+        <p>Ti consigliamo di farlo una volta a settimana (per esempio, il noiosissimo lunedì).</p>
+    </div>
+    <div class="row justify-content-center">
+        <button type="submit" class="btn btn-link" name="register-total">
+            <?php
+                $register_values = "php/update_statistic.php?register=true&trend=".$total_trend."&min=".$total_min."&album=".$id_album;
+                echo '<a class="btn text-white" style="background-color: #5401a7;" href="'.$register_values.'">Register new values</a> '; 
+            ?>
+        </button>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+        <h5><p class="font-weight-bold">Esporta l'album.</p></h5>
+    </div>
+    <div class="row justify-content-center">
+        <p>Cliccando sul bottone sottostante, viene scaricato un file .txt contente i dati della tabella dell'album, in modo tale da poterlo condividere.</p>
+    </div>
+    <div class="row justify-content-center">
+        <button type="submit" class="btn text-white" style="background-color: #5401a7;" name="register-total"> Esporta Album </button>
+    </div>
+   
+   
+
+
+
+
+
+<!-- Grafico che mostra i valori dell'album -->
 
 <?php
 
 $sql = "SELECT Stat_date, Trend_value, Min_value FROM statistic WHERE Idalbum = ? ";
 $stmt = mysqli_stmt_init($connessione);
-if(!mysqli_stmt_prepare($stmt, $sql)){
+if(!mysqli_stmt_prepare($stmt, $sql)) {
      echo "Error in the database";
 }
 else{
@@ -274,11 +341,6 @@ mysqli_close($connessione);
 </div>
 
 
-
-<?php   
-   ////   SCRIPT TO UPLOADS IN THE DATABASE THE VALUES
-
-?>
 
 
 
@@ -564,178 +626,6 @@ mysqli_close($connessione);
 ?>
 
 
-<?php
-
-    function low_trend($idcarta){
-
-        $id_product = $idcarta;
-
-
-        $method             = "GET";
-        $url                = "https://api.cardmarket.com/ws/v2.0/output.json/products/".$id_product;
-        $appToken           = "D5lSR859bgB50sVj";
-        $appSecret          = "DLszKXEZCrNbZRQ8dTc1kLo6QxyDkicR";
-        $accessToken        = "";
-        $accessSecret       = "";
-        $nonce              = "53eb1f44909d6";
-        $timestamp          = "1407917892";
-        $signatureMethod    = "HMAC-SHA1";
-        $version            = "1.0";
-
-        /**
-        * Gather all parameters that need to be included in the Authorization header and are know yet
-        *
-        * Attention: If you have query parameters, they MUST also be part of this array!
-        *
-        * @var $params array|string[] Associative array of all needed authorization header parameters
-        */
-        $params             = array(
-        'realm'                     => $url,
-        'oauth_consumer_key'        => $appToken,
-        'oauth_token'               => $accessToken,
-        'oauth_nonce'               => $nonce,
-        'oauth_timestamp'           => $timestamp,
-        'oauth_signature_method'    => $signatureMethod,
-        'oauth_version'             => $version,
-        );
-
-        /**
-        * Start composing the base string from the method and request URI
-        *  $url    = "https://api.cardmarket.com/ws/v2.0/articles/".$id_product. "&idLanguage=".$language."&minCondition=".$cond."&start=0&maxResults=10";
-        * Attention: If you have query parameters, don't include them in the URI
-        *
-        * @var $baseString string Finally the encoded base string for that request, that needs to be signed
-        */
-        $baseString         = strtoupper($method) . "&";
-        $baseString        .= rawurlencode($url) . "&";
-
-        /*
-        * Gather, encode, and sort the base string parameters
-        */
-        $encodedParams      = array();
-        foreach ($params as $key => $value)
-        {
-        if ("realm" != $key)
-        {
-            $encodedParams[rawurlencode($key)] = rawurlencode($value);
-        }
-        }
-        ksort($encodedParams);
-
-        /*
-        * Expand the base string by the encoded parameter=value pairs
-        */
-        $values             = array();
-        foreach ($encodedParams as $key => $value)
-        {
-        $values[] = $key . "=" . $value;
-        }
-        $paramsString       = rawurlencode(implode("&", $values));
-        $baseString        .= $paramsString;
-
-        /*
-        * Create the signingKey
-        */
-        $signatureKey       = rawurlencode($appSecret) . "&" . rawurlencode($accessSecret);
-
-        /**
-        * Create the OAuth signature
-        * Attention: Make sure to provide the binary data to the Base64 encoder
-        *
-        * @var $oAuthSignature string OAuth signature value
-        */
-        $rawSignature       = hash_hmac("sha1", $baseString, $signatureKey, true);
-        $oAuthSignature     = base64_encode($rawSignature);
-
-        /*
-        * Include the OAuth signature parameter in the header parameters array
-        */
-        $params['oauth_signature'] = $oAuthSignature;
-
-        /*
-        * Construct the header string
-        */
-        $header             = "Authorization: OAuth ";
-        $headerParams       = array();
-        foreach ($params as $key => $value)
-        {
-        $headerParams[] = $key . "=\"" . $value . "\"";
-        }
-        $header            .= implode(", ", $headerParams);
-
-        /*
-        * Get the cURL handler from the library function
-        */
-        $curlHandle         = curl_init();
-
-        /*
-        * Set the required cURL options to successfully fire a request to MKM's API
-        *
-        * For more information about cURL options refer to PHP's cURL manual:
-        * http://php.net/manual/en/function.curl-setopt.php
-        */
-        $url = "https://api.cardmarket.com/ws/v2.0/output.json/products/".$id_product;
-
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlHandle, CURLOPT_URL, $url);
-        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array($header));
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
-
-        /**
-        * Execute the request, retrieve information about the request and response, and close the connection
-        *
-        * @var $content string Response to the request
-        * @var $info array Array with information about the last request on the $curlHandle
-        */
-        $content            = curl_exec($curlHandle);
-        $info               = curl_getinfo($curlHandle);
-        curl_close($curlHandle);
-
-
-        //$decoded            = json_decode($content);
-        //$decoded            = simplexml_load_string($content);
-
-        //echo "Contenuto  ". $content;
-        //echo "Informazioni  ";
-        //print_r($info );
-
-
-
-        $jsonIterator = new RecursiveIteratorIterator(
-        new RecursiveArrayIterator(json_decode($content, TRUE)),
-        RecursiveIteratorIterator::SELF_FIRST);
-
-        $low_trend = array();
-
-        foreach ($jsonIterator as $key => $val) {
-            if(is_array($val)) {
-                
-            } else {
-                
-                if($key == "LOW"){
-                        array_push($low_trend, $val);
-                }
-                if($key == "TREND"){
-                    array_push($low_trend, $val);
-                }
-                if($key == "website"){
-                    array_push($low_trend, $val);
-                   }
-                  
-                  
-                  
-              }
-            }
-        $uncomplete_link = $low_trend[0];
-        $complete_link = "https://www.cardmarket.com".$uncomplete_link ;
-        $low_trend[0] = $complete_link;
-
-        return $low_trend;
-
-}//chiusura funzione
-
-
-?>
 
 
 
@@ -757,5 +647,31 @@ mysqli_close($connessione);
 
     }
 
+?>
+
+<?php
+    function check_album_registration($id_album){
+
+        // Controlla se l'album è già registrato nella tabella Statistic. In questo modo possiamo abilitareo meno
+        // il pulsante "Start register"
+        require "php/dbh.php";
+        $sql = "SELECT Idalbum FROM statistic WHERE Idalbum = ? ";
+        $stmt = mysqli_stmt_init($connessione);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            echo "SQL error";
+        }
+        else{
+
+            mysqli_stmt_bind_param($stmt, "i", $id_album);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if ($result->num_rows > 0) {
+                return true; //l'album è registrato
+            } else {
+                return false; //l'album non è registrato
+            }
+        }
+    }
 ?>
 
