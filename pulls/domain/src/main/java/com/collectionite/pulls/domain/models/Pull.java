cb6cc3a3bd4038213d069836d549f8d7512e069d
Leonardo Carlassare <lio.del.bronx@gmail.com>
2023-12-25 17:54:22 +0100

    feat: added domain module

---
+++ pulls/domain/src/main/java/com/collectionite/pulls/domain/models/Pull.java
@@ -0,0 +1,20 @@
+package com.collectionite.pulls.domain.models;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Pull {
+
+    private String pullId;
+    private Integer cardsNumber;
+    private List<Integer> cardsIds;
+    private Integer expansionId;
+    private Integer gameId;
+}
