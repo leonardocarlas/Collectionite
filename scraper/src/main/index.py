62a74f3c80a1739b440bc152ecc52f7767781249
Leonardo Carlassare <lio.del.bronx@gmail.com>
2024-06-17 12:07:09 +0200

    feat: updated index.py

--- scraper/src/main/index.py
+++ scraper/src/main/index.py
@@ -3,27 +3,14 @@ from service import get_html_content_from_url
 from bs4 import BeautifulSoup
 from typing import List
 
-
-def getUrls(base_url: str) -> List[str]:
-    return [
-        "https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site=1", 
-        "https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site=2"
-    ]
-
-for i in range(1,13):
-    url: str = f"https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site={i}"
-    print(url)
+from src.main.utils import link_generator
 
 if __name__ == "__main__":
     print("Collection Scraper")
-    # Example usage
-    url = "https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site=13"
-    html_content = get_html_content_from_url(url)
-    print(html_content)
-    # urls: List[str] = getUrls("https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site=")
-    document: str = get_html_content_from_url("test")
-    print(document)
-    soup = BeautifulSoup(document, 'html.parser')
-    print(soup)
+    links: List[str] = link_generator("https://www.cardmarket.com/en/Pokemon/Products/Singles/Temporal-Forces?idCategory=51&idExpansion=5589&idRarity=0&sortBy=collectorsnumber_asc&site=",2)
+    for link in links:
+        document: str = get_html_content_from_url(link)
+        print(document)
+        soup = BeautifulSoup(document, 'html.parser')
 
 
