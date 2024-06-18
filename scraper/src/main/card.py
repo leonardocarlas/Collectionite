a13a2c3a8ff5fcedd293639529e2d97db2e02781
Leonardo Carlassare <lio.del.bronx@gmail.com>
2024-06-18 14:37:03 +0200

    feat: added constants.py

--- scraper/src/main/card.py
+++ scraper/src/main/card.py
@@ -1,10 +1,13 @@
+from src.main.constants import Constants
+
+
 class Card:
 
     def __init__(self, id_card: str, card_name: str, link_image: str, marketplace_link: str):
         self.id_card = id_card
         self.card_name = card_name
         self.link_image = link_image
-        self.marketplace_link = marketplace_link
+        self.marketplace_link = Constants.MARKETPLACE_DOMAIN + marketplace_link
 
     def test_url(self):
         print(f"Link to cardmarket: {self.marketplace_link}")
